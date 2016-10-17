a0df8a4db598a7cf2de5f0fc2c107d6e00fe1e36
Cedric Charly <cedric.charly@gmail.com>
2016-10-17 19:35:34 +0000

    Add configuration values used by Flask

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ import os
 
 # Determine code location in file system
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_FILES = os.path.join(BASE_DIR, 'static')
+TEMPLATES = os.path.join(BASE_DIR, 'templates')
 
-# Database URI used by SQLAlchemy
+# Database configuration used by SQLAlchemy and flask_sqlalchemy
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask configuration: see http://flask.pocoo.org/docs/latest/config/
 
