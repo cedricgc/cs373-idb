94d3994b99c251cc5077695287c1f6f1d7e9849e
Ali <alitarekrashed@gmail.com>
2016-10-18 17:41:02 -0500

    Instance page displays a pokemon's attributes
    References #15

--- static/js/pokemon_info.js
+++ static/js/pokemon_info.js
@@ -1,12 +1,18 @@
-angular.module('pokemonApp', [])
-    .controller('pokemonController', function($scope) {
+angular.module('pokemonInfoApp', [])
+    .controller('pokemonInfoController', function($scope) {
         $scope.pokemon = [
             {id: 1, name: "Bulbasaur", flavor_text: "Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun\u2019s rays,\nthe seed grows progressively larger", habitat: "grassland", color: "green", shape: "quadruped"},
             {id: 4, name: "Charmander", flavor_text: "The flame that burns at the tip of its tail is an indication\nof its emotions. The flame wavers when Charmander\nis enjoying itself. If the Pok\u00e9mon becomes enraged,\nthe flame burns fiercely.", habitat: "mountain", color: "red", shape: "upright"},
             {id: 7, name: "Squirtle", flavor_text: "Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun\u2019s rays,\nthe seed grows progressively larger", habitat: "waters-edge", color: "blue", shape: "upright"},
         ];
 
-		$scope.goToPokemonInstance = function (pokemon_id){
-			location.href = '/pokemon_info/' + pokemon_id ;
+        $scope.instance = [];
+
+		$scope.initInstance = function (pokemon_id){
+			angular.forEach($scope.pokemon, function(value, index){
+				if(value["id"] == pokemon_id) {
+					$scope.instance.push(value);
+				}
+			});
 		}
 });
