016882f1c2ab5be417e9b0bbdb16ba16c1109dc1
Ali <alitarekrashed@gmail.com>
2016-10-18 18:34:12 -0500

    Accidentally listed wrong description for squirtle

--- static/js/pokemon.js
+++ static/js/pokemon.js
@@ -3,7 +3,7 @@ angular.module('pokemonApp', [])
         $scope.pokemon = [
             {id: 1, name: "Bulbasaur", flavor_text: "Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun\u2019s rays,\nthe seed grows progressively larger", habitat: "grassland", color: "green", shape: "quadruped"},
             {id: 4, name: "Charmander", flavor_text: "The flame that burns at the tip of its tail is an indication\nof its emotions. The flame wavers when Charmander\nis enjoying itself. If the Pok\u00e9mon becomes enraged,\nthe flame burns fiercely.", habitat: "mountain", color: "red", shape: "upright"},
-            {id: 7, name: "Squirtle", flavor_text: "Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun\u2019s rays,\nthe seed grows progressively larger", habitat: "waters-edge", color: "blue", shape: "upright"},
+            {id: 7, name: "Squirtle", flavor_text: "Squirtle\u2019s shell is not merely used for protection.\nThe shell\u2019s rounded shape and the grooves on its\nsurface help minimize resistance in water,\nenabling this Pok\u00e9mon to swim at high speeds.", habitat: "waters-edge", color: "blue", shape: "upright"}
         ];
 
 		$scope.goToPokemonInstance = function (pokemon_id){
