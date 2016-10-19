5220fb4d640c73c7a1542e1cf99d99328cba124d
Ali <alitarekrashed@gmail.com>
2016-10-19 02:00:29 -0500

    Basic sorting on a table in descending order
    References #17

--- static/js/pokemon.js
+++ static/js/pokemon.js
@@ -1,6 +1,7 @@
 angular.module('pokemonApp', [])
     .controller('pokemonController', function($scope, $http) {
-    	
+    	$scope.sortPokemon = 'id';
+    	$scope.reversePokemon = false;
     	$http.get('static/testdata.json').success(function(data) {
     		$scope.pokemon = data["pokemon"];
     	});
