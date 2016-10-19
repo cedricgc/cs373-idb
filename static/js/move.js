d9341cc102eb2a126cb3bdb07fece2040607343e
Ali <alitarekrashed@gmail.com>
2016-10-19 01:06:23 -0500

    Adds pokemon to moves instance page
    References $15

--- static/js/move.js
+++ static/js/move.js
@@ -1,16 +1,25 @@
 angular.module('moveApp', [])
     .controller('moveController', function($scope, $http, $window) {
-        $scope.instance = [];
-
+        $scope.moves = [];
+        $scope.pokemon = [];
         $scope.initInstance = function (move_id){
             $http.get('../static/testdata.json')
                 .success(function(data) {
-                $scope.moves = data["moves"];
-                angular.forEach($scope.moves, function(value, index){
+                var all_moves = data["moves"];
+                var all_pokemon = data["pokemon"];
+                angular.forEach(all_moves, function(value, index){
                     if(value["id"] == move_id) {
-                        $scope.instance.push(value);
+                        $scope.moves.push(value);
+                    }
+                });
+
+                angular.forEach(all_pokemon, function(value, index){
+                    var pokemon_id_index = $scope.moves[0]["pokemon"].indexOf(value["id"]);
+                    if(pokemon_id_index != -1) {
+                        $scope.pokemon.push(value);
                     }
                 });
             });
+
         }
 });
