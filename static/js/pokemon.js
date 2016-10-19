32fafbf44f2313867b9c5e510742b6de431d10b4
Ali <alitarekrashed@gmail.com>
2016-10-19 18:03:33 -0500

    Ascending/Descending sorting for one of the tables

--- static/js/pokemon.js
+++ static/js/pokemon.js
@@ -1,8 +1,37 @@
 angular.module('pokemonApp', [])
     .controller('pokemonController', function($scope, $http) {
-    	$scope.sortPokemon = 'id';
-    	$scope.reversePokemon = false;
     	$http.get('static/testdata.json').success(function(data) {
     		$scope.pokemon = data["pokemon"];
     	});
+
+        $scope.sortTerm = 'id';
+        $scope.reverse = false;
+
+        $scope.switch = function (column_name) {
+            console.log("switching to " + column_name)
+            if(column_name != $scope.sortTerm) {
+                $scope.sortTerm = column_name;
+                $scope.reverse = false;
+            } else {
+                $scope.reverse = !$scope.reverse;
+            }
+        }
+
+        $scope.isAscending = function(column_name) {
+            if(column_name == $scope.sortTerm && $scope.reverse) {
+                console.log("True!!!");
+                return true;
+            }
+
+            return false;
+        }
+
+        $scope.isDescending = function(column_name) {
+            if(column_name == $scope.sortTerm && !$scope.reverse) {
+                return true;
+            }
+
+            return false;
+        }
+
 	});
