47da64fcfb2c475cb904504231fd9a60e79a53ba
Ali <alitarekrashed@gmail.com>
2016-10-19 00:58:48 -0500

    Updated pokemon instance page to other models
    The page for a Pokemon instance now shows the Pokedexes the
    Pokemon appears in, as well as the moves the Pokemon has.
    This references #15.

--- static/js/pokemon_info.js
+++ static/js/pokemon_info.js
@@ -1,14 +1,33 @@
 angular.module('pokemonInfoApp', [])
     .controller('pokemonInfoController', function($scope, $http) {
-        $scope.instance = [];
-
+        $scope.pokemon = [];
+        $scope.pokedexes = [];
+        $scope.moves = [];
         $scope.initInstance = function (pokemon_id){
             $http.get('../static/testdata.json')
                 .success(function(data) {
-                $scope.pokemon = data["pokemon"];
-                angular.forEach($scope.pokemon, function(value, index){
+                var all_pokemon = data["pokemon"];
+                var all_pokedexes = data["pokedexes"];
+                var all_moves = data["moves"];
+                angular.forEach(all_pokemon, function(value, index){
                     if(value["id"] == pokemon_id) {
-                        $scope.instance.push(value);
+                        $scope.pokemon.push(value);
+                    }
+                });
+
+                angular.forEach(all_pokedexes, function(value, index){
+                	var pokedex_id_index = $scope.pokemon[0]["pokedexes"].indexOf(value["id"]);
+                	console.log(pokedex_id_index);
+                    if(pokedex_id_index != -1) {
+                        $scope.pokedexes.push(value);
+                    }
+                });
+
+                angular.forEach(all_moves, function(value, index){
+                	var move_id_index = $scope.pokemon[0]["moves"].indexOf(value["id"]);
+                	console.log(move_id_index);
+                    if(move_id_index != -1) {
+                        $scope.moves.push(value);
                     }
                 });
             });
