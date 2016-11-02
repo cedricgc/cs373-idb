a30f0dbd3a8526e6a8b5579eef9ba5a3cf095390
Ali <alitarekrashed@gmail.com>
2016-11-02 01:15:47 -0500

    pagination elements on pokedex table
    Currently tracking the current page, and added logic for
    prev/next pages, skipping pages, etc. currently the total number
    of pages is hardcoded. References #34

--- static/js/index_pokedexes.js
+++ static/js/index_pokedexes.js
@@ -1,8 +1,42 @@
 angular.module('pokedexesApp', ['tableApp'])
   .controller('pokedexesController', function($scope, $http, tableService) {
+    $scope.currentPage = 4;
+    $scope.totalPages = 10;
+
+
     $http.get('/static/testdata/pokedexes.json').success(function(data) {
       $scope.pokedexes = data["data"];
     });
+
+    $scope.pageRange = function () {
+      if ($scope.currentPage <= 2) {
+        return [1, 2, 3];
+      } else if ($scope.totalPages - $scope.currentPage <= 2) {
+        return [$scope.totalPages - 2, $scope.totalPages - 1, $scope.totalPages];
+      } else {
+        return [$scope.currentPage - 1, $scope.currentPage, $scope.currentPage + 1];
+      }
+    }
+
+    $scope.nextPage = function() {
+      if($scope.currentPage < $scope.totalPages){
+        $scope.currentPage++;
+      }
+    }
+
+    $scope.prevPage = function() {
+      if($scope.currentPage > 1){
+        $scope.currentPage--;
+      }
+    }
+
+    $scope.skipBack = function() {
+      $scope.currentPage -= 2;
+    }
+
+    $scope.skipForward = function() {
+      $scope.currentPage += 2;
+    }
 
     var tableVars = {"sortTerm" : 'id', "reverse" : false};
 
