1bb3f8c4a284e3d647833b59231a572608ba4a81
Cedric Charly <cedric.charly@gmail.com>
2016-11-03 03:36:03 +0000

    Change test_models to use pytest conventions
    closes #45

--- tests/unit/api/test_models.py
+++ tests/unit/api/test_models.py
@@ -1,109 +1,101 @@
 # -*- coding: utf-8 -*-
 
 
-import unittest
+import pytest
 
 import website.api.models as models
 
-pokedex = {
-    'id': 1,
-    'name': 'kanto',
-    'official_name': 'Original Kanto',
-    'region': 'Kanto',
-    'description': 'This is a list of Pokémon in the order dictated by the Kanto regional Pokédex'
-}
 
-pokemon = {
-    'id': 1,
-    'name': 'Bulbasaur',
-    'flavor_text': 'A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.',
-    'habitat': 'grassland',
-    'color': 'green',
-    'shape': 'quadruped'
-}
+@pytest.fixture
+def pokedex():
+    pokedex = {
+        'id': 1,
+        'name': 'kanto',
+        'official_name': 'Original Kanto',
+        'region': 'Kanto',
+        'description': 'This is a list of Pokémon in the order dictated by the Kanto regional Pokédex'
+    }
 
-move = {
-    'id': 1,
-    'name': 'Razor Wind',
-    'flavor_text': 'A two-turn attack. Blades of wind hit opposing Pokémon on the second turn. Critical hits land more easily.',
-    'short_effect': 'Requires a turn to charge before attacking.',
-    'effect': 'Inflicts regular damage.  User\'s critical hit rate is one level higher when using this move.  User charges for one turn before attacking. This move cannot be selected by sleep talk.',
-    'damage_class': 'special',
-    'power_points': 10,
-    'power': 80,
-    'accuracy': 100,
-}
+    return models.Pokedex(**pokedex)
 
 
-class TestPokedex(unittest.TestCase):
+@pytest.fixture
+def pokemon():
+    pokemon = {
+        'id': 1,
+        'name': 'Bulbasaur',
+        'flavor_text': 'A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.',
+        'habitat': 'grassland',
+        'color': 'green',
+        'shape': 'quadruped'
+    }
 
-    def test_create_pokedex(self):
-        pd = models.Pokedex(**pokedex)
-
-        self.assertEqual(pd.id, 1)
-        self.assertEqual(pd.name, 'kanto')
-
-    def test_pokedex_repr(self):
-        pd = models.Pokedex(**pokedex)
-
-        self.assertEqual(repr(pd), '<Pokedex kanto>')
-
-    def test_pokedex_relationship(self):
-        pd = models.Pokedex(**pokedex)
-        po = models.Pokemon(**pokemon)
-        pd.pokemon = [po]
-
-        self.assertEqual(pd.pokemon, [po])
+    return models.Pokemon(**pokemon)
 
 
-class TestPokemon(unittest.TestCase):
+@pytest.fixture
+def move():
+    move = {
+        'id': 1,
+        'name': 'Razor Wind',
+        'flavor_text': 'A two-turn attack. Blades of wind hit opposing Pokémon on the second turn. Critical hits land more easily.',
+        'short_effect': 'Requires a turn to charge before attacking.',
+        'effect': 'Inflicts regular damage.  User\'s critical hit rate is one level higher when using this move.  User charges for one turn before attacking. This move cannot be selected by sleep talk.',
+        'damage_class': 'special',
+        'power_points': 10,
+        'power': 80,
+        'accuracy': 100,
+    }
 
-    def test_create_pokemon(self):
-        po = models.Pokemon(**pokemon)
-
-        self.assertEqual(po.id, 1)
-        self.assertEqual(po.name, 'Bulbasaur')
-
-    def test_pokemon_repr(self):
-        po = models.Pokemon(**pokemon)
-
-        self.assertEqual(repr(po), '<Pokemon Bulbasaur>')
-
-    def test_pokemon_relationship(self):
-        po = models.Pokemon(**pokemon)
-        pd = models.Pokedex(**pokedex)
-        po.pokedexes = [pd]
-
-        self.assertEqual(po.pokedexes, [pd])
-
-    def test_pokemon_relationship_2(self):
-        po = models.Pokemon(**pokemon)
-        m = models.Move(**move)
-        po.moves = [m]
-
-        self.assertEqual(po.moves, [m])
+    return models.Move(**move)
 
 
-class TestMove(unittest.TestCase):
-
-    def test_create_move(self):
-        m = models.Move(**move)
-
-        self.assertEqual(m.id, 1)
-        self.assertEqual(m.name, 'Razor Wind')
-
-    def test_move_repr(self):
-        m = models.Move(**move)
-
-        self.assertEqual(repr(m), '<Move Razor Wind>')
-
-    def test_move_relationship(self):
-        m = models.Move(**move)
-        po = models.Pokemon(**pokemon)
-        m.pokemon = [po]
-
-        self.assertEqual(m.pokemon, [po])
+def test_pokedex_create(pokedex):
+    assert pokedex.id == 1
+    assert pokedex.name == 'kanto'
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_pokedex_repr(pokedex):
+    assert repr(pokedex) == '<Pokedex kanto>'
+
+
+def test_pokedex_relationship(pokedex, pokemon):
+    pokedex.pokemon = [pokemon]
+
+    assert pokedex.pokemon == [pokemon]
+
+
+def test_pokemon_create(pokemon):
+    assert pokemon.id == 1
+    assert pokemon.name == 'Bulbasaur'
+
+
+def test_pokemon_repr(pokemon):
+    assert repr(pokemon) == '<Pokemon Bulbasaur>'
+
+
+def test_pokemon_relationship(pokemon, pokedex):
+    pokemon.pokedexes = [pokedex]
+
+    assert pokemon.pokedexes == [pokedex]
+
+
+def test_pokemon_relationship_2(pokemon, move):
+    pokemon.moves = [move]
+
+    assert pokemon.moves == [move]
+
+
+def test_move_create(move):
+    assert move.id == 1
+    assert move.name == 'Razor Wind'
+
+
+def test_move_repr(move):
+    assert repr(move) == '<Move Razor Wind>'
+
+
+def test_move_relationship(move, pokemon):
+    move.pokemon = [pokemon]
+
+    assert move.pokemon == [pokemon]
