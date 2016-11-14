e2aeab046d0744f45355d2a059168cc6336930ff
Cedric Charly <cedric.charly@gmail.com>
2016-11-14 01:31:34 +0000

    Update migrations for sqlalchemy_search triggers

--- alembic/versions/6914bc522f93_create_pokedexes_table.py
+++ alembic/versions/6914bc522f93_create_pokedexes_table.py
@@ -8,12 +8,17 @@ Create Date: 2016-10-16 17:03:58.272820
 from alembic import op
 import sqlalchemy as sa
 
+from sqlalchemy_searchable import sync_trigger
+from sqlalchemy_utils import TSVectorType
+
 
 # revision identifiers, used by Alembic.
 revision = '6914bc522f93'
 down_revision = None
 branch_labels = None
 depends_on = None
+
+searchable = ['name', 'official_name', 'region', 'description']
 
 
 def upgrade():
@@ -25,12 +30,17 @@ def upgrade():
         sa.Column('region', sa.Text, nullable=True, default=None),
         sa.Column('description', sa.Text, nullable=True, default=None),
 
+        sa.Column('search_vector', TSVectorType(searchable)),
+
         sa.Column('inserted_at', sa.DateTime,
                   default=sa.func.current_timestamp(), nullable=False),
         sa.Column('updated_at', sa.DateTime,
                   default=sa.func.current_timestamp(),
                   onupdate=sa.func.current_timestamp(), nullable=False)
     )
+
+    conn = op.get_bind()
+    sync_trigger(conn, 'pokedexes', 'search_vector', searchable)
 
 
 def downgrade():
