0c7009fb9e29669a9380b90f13f7a38ad1031d27
samuelwilliamson77 <samuelwilliamson77@gmail.com>
2016-11-15 03:54:59 +0000

    pokedexes and pokemon linked while scraping

--- scripts/scraper/load_data.py
+++ scripts/scraper/load_data.py
@@ -36,52 +36,58 @@ def field_extractor(obj, field, field2):
 
 
 def clean_pokedexes(api_pokedexes):
-    cleaned = []
+    cleaned = {}
     for pokedex in api_pokedexes:
         region = pokedex.get('region', None)
         if region is not None:
             region = region['name']
+        id = pokedex['id']
         clean_pokedex = {
+            'id': id,
             'name': pokedex['name'],
             'official_name': field_extractor(pokedex, 'name', 'names'),
             'region': region,
-            'description': field_extractor(pokedex, 'description', 'descriptions')
+            'description': field_extractor(pokedex, 'description', 'descriptions'),
+            'pokemon': []
         }
-
-        cleaned.append(clean_pokedex)
-
+        cleaned[id] = clean_pokedex
     return cleaned
 
 
 def clean_pokemon(api_pokemon):
-    cleaned = []
+    cleaned = {}
     for pokeman in api_pokemon:
         habitat = pokeman.get('habitat', None)
         if habitat is not None:
             habitat = habitat['name']
+        id = pokeman['id']
         clean_pokeman = {
+            'id': id,
             'name': field_extractor(pokeman, 'name', 'names'),
             'flavor_text': field_extractor(pokeman, 'flavor_text', 'flavor_text_entries'),
             'habitat': habitat,
             'color': pokeman['color']['name'],
-            'shape': pokeman['shape']['name']
+            'shape': pokeman['shape']['name'],
+            'pokedexes': [],
+            'moves': []
         }
-
-        cleaned.append(clean_pokeman)
+        cleaned[id] = clean_pokeman
 
     return cleaned
 
 
 def clean_moves(api_moves):
-    cleaned = []
+    cleaned = {}
     for move in api_moves:
-        damage_class = move.get('damage_class', None)
+        damage_class    = move.get('damage_class', None)
         if damage_class is not None:
             damage_class = damage_class['name']
-        power_points = move.get('pp', None)
-        power = move.get('power', None)
-        accuracy = move.get('accuracy', None)
+        id = move['id']
+        power_points    = move.get('pp', None)
+        power           = move.get('power', None)
+        accuracy        = move.get('accuracy', None)
         clean_move = {
+            'id': id,
             'name': field_extractor(move, 'name', 'names'),
             'flavor_text': field_extractor(move, 'flavor_text', 'flavor_text_entries'),
             'short_effect': move['effect_entries'][0]['short_effect'],
@@ -89,68 +95,105 @@ def clean_moves(api_moves):
             'damage_class': damage_class,
             'power_points': power_points,
             'power': power,
-            'accuracy': accuracy
+            'accuracy': accuracy,
+            'pokemon': []
         }
-
-        cleaned.append(clean_move)
+        cleaned[id] = clean_move
 
     return cleaned
 
 
+def assoc_pokedexes_pokemon(
+    pokedex_dict, pokemon_dict,  pokemon_api):
+    
+    pokedex_lookup = { pdex['name']: pdex['id'] 
+        for pdex in pokedex_dict.values() }
+    
+    for pmon in pokemon_api:
+        id = pmon['id']
+        for entry in pmon['pokedex_numbers']:
+            pdex_name = entry['pokedex']['name']
+            num = pokedex_lookup[ pdex_name ]
+            
+            pokemon_list = pokedex_dict[num]['pokemon']
+            pokedex_list = pokemon_dict[id]['pokedexes']
+
+            pokemon_list.append(id)
+            pokedex_list.append(num)
+    
 def main():
-    data_files = sys.argv
+    data_files = sys.argv  
+
+    pokedexes = {}
+    pokemon = {}
+    moves = {}
+
+    print('READING POKEDEX JSON')
+    with open(data_files[1]) as data:
+        pokedex_api = json.load(data)
+        pokedexes = clean_pokedexes(pokedex_api)
+
+    print('READING POKEMON JSON')
+    with open(data_files[2]) as data:
+        pokemon_api = json.load(data)
+        pokemon = clean_pokemon(pokemon_api)
+
+    print('READING MOVE JSON')
+    with open(data_files[3]) as data:
+        move_api = json.load(data)
+        moves = clean_moves(move_api)
+
+    print('ASSOCIATING POKEDEXES AND POKEMON')
+    with open(data_files[2]) as data:
+        pokemon_api = json.load(data)
+        assoc_pokedexes_pokemon( pokedexes, pokemon, pokemon_api )
 
     print('STARTING POKEDEX INSERTS')
-    with open(data_files[1]) as data:
-        pokedexes = json.load(data)
-        cleaned = clean_pokedexes(pokedexes)
-        for pd in cleaned:
-            pokedex, errors = pokedex_schema.load(pd)
-            print(pokedex)
-            print(errors)
-            try:
-                db.session.add(pokedex)
-                db.session.commit()
-            except sqlalchemy.exc.SQLAlchemyError as e:
-                print("Failed to insert pokedex data")
-                return 1
+    for pd in pokedexes.values():
+        pokedex, errors = pokedex_schema.load(pd)
+        #print(pokedex)
+        #print(errors)
+        try:
+            db.session.add(pokedex)
+            db.session.commit()
+            print()
+        except sqlalchemy.exc.SQLAlchemyError as e:
+            print("Failed to insert pokedex data")
+            return 1
     print('FINISHED POKEDEX INSERTS')
 
     print('STARTING POKEMON INSERTS')
-    with open(data_files[2]) as data:
-        pokemon = json.load(data)
-        cleaned = clean_pokemon(pokemon)
-        for po in cleaned:
-            pokeman, errors = pokemon_schema.load(po)
-            print(pokeman)
-            print(errors)
-            try:
-                db.session.add(pokeman)
-                db.session.commit()
-            except sqlalchemy.exc.SQLAlchemyError as e:
-                print("Failed to insert pokemon data")
-                return 1
+    for po in pokemon.values():
+        pokeman, errors = pokemon_schema.load(po)
+        #print(pokeman)
+        #print(errors)
+        try:
+            db.session.add(pokeman)
+            db.session.commit()
+            print()
+        except sqlalchemy.exc.SQLAlchemyError as e:
+            print("Failed to insert pokemon data")
+            return 1
     print('FINISHED POKEMON INSERTS')
 
     print('STARTING MOVE INSERTS')
-    with open(data_files[3]) as data:
-        moves = json.load(data)
-        cleaned = clean_moves(moves)
-        for mv in cleaned:
-            print(mv)
-            move, errors = move_schema.load(mv)
-            print(move)
-            print(errors)
-            try:
-                db.session.add(move)
-                db.session.commit()
-            except sqlalchemy.exc.SQLAlchemyError as e:
-                print("Failed to insert move data")
-                return 1
+    for mv in moves.values():
+        move, errors = move_schema.load(mv)
+        #print(move)
+        #print(errors)
+        try:
+            db.session.add(move)
+            db.session.commit()
+            print()
+        except sqlalchemy.exc.SQLAlchemyError as e:
+            print("Failed to insert move data")
+            return 1
     print('FINISHED MOVE INSERTS')
+    
+    for pdex in pokedexes.values():
+        print(pdex, end='\n\n')
 
     return 0
-
 
 if __name__ == '__main__':
     return_code = main()
